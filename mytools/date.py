5d04ce2491156ca421b6b5ccd81846ffc7576aea
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-10 07:30:03 +0100

    [lib] add date_to_day_of_year

--- mytools/date.py
+++ mytools/date.py
@@ -21,6 +21,15 @@ def date_to_string(date: Union[datetime.datetime, typeIterable[datetime.datetime
         return date.strftime(date_format)
 
 
+def date_to_day_of_year(date: Union[str, list], date_format: str = '%m/%d/%y'):
+    if isinstance(date, list):
+        return list(date_to_day_of_year(d, date_format) for d in date)
+    else:
+        d = datetime.datetime.strptime(date, date_format)
+        delta = d - datetime.datetime(d.year, 1, 1)
+        return delta.days + 1
+
+
 def day_of_year_to_string(day: Union[int, float, typeIterable], date_format: str = '%d %b') -> Union[
     str, typeIterable[str]]:
     if isinstance(day, Iterable):
