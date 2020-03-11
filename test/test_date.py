0e7b9710f96ae17e8e054a589c1d685435ba2a3d
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-11 12:00:25 +0100

    [test] add str_to_day_of_year and day_of_year_to_date

--- test/test_date.py
+++ test/test_date.py
@@ -5,17 +5,37 @@ import datetime
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.year = 2011
+        self.format = '%d/%m/%y'
+        self.this_year = int(datetime.datetime.now().year)
+        self.date_list = [datetime.datetime(self.year, 1, 1, 0),
+                          datetime.datetime(self.year, 2, 1, 0),
+                          datetime.datetime(self.year, 2, 4, 0)]
+        self.date_list_this_year = [datetime.datetime(self.this_year, 1, 1, 0),
+                                    datetime.datetime(self.this_year, 2, 1, 0),
+                                    datetime.datetime(self.this_year, 2, 4, 0)]
+        self.days_list = [1, 32, 35]
+        self.str_date_list = [d.strftime(self.format) for d in self.date_list]
+
     def test_day_of_year_to_date(self):
-        year = 2011
-        this_year = int(datetime.datetime.now().year)
-        date_list = [datetime.datetime(year, 1, 1, 0), datetime.datetime(year, 2, 1, 0), datetime.datetime(year, 2, 4, 0)]
-        date_list_this_year = [datetime.datetime(this_year, 1, 1, 0), datetime.datetime(this_year, 2, 1, 0), datetime.datetime(this_year, 2, 4, 0)]
-        days_list = [1, 32, 35]
-        self.assertEqual(dt.day_of_year_to_date(days_list[0], year), date_list[0])
-        self.assertEqual(dt.day_of_year_to_date(days_list[1], year), date_list[1])
-        self.assertEqual(dt.day_of_year_to_date(days_list[2], year), date_list[2])
-        self.assertListEqual(dt.day_of_year_to_date(days_list, year), date_list)
-        self.assertListEqual(dt.day_of_year_to_date(days_list), date_list_this_year)
+        self.assertEqual(dt.day_of_year_to_date(self.days_list[0], self.year), self.date_list[0])
+        self.assertEqual(dt.day_of_year_to_date(self.days_list[1], self.year), self.date_list[1])
+        self.assertEqual(dt.day_of_year_to_date(self.days_list[2], self.year), self.date_list[2])
+        self.assertListEqual(dt.day_of_year_to_date(self.days_list, self.year), self.date_list)
+        self.assertListEqual(dt.day_of_year_to_date(self.days_list), self.date_list_this_year)
+
+    def test_date_to_day_of_year(self):
+        self.assertEqual(dt.date_to_day_of_year(self.date_list[0]), self.days_list[0])
+        self.assertEqual(dt.date_to_day_of_year(self.date_list[1]), self.days_list[1])
+        self.assertEqual(dt.date_to_day_of_year(self.date_list[2]), self.days_list[2])
+        self.assertListEqual(dt.date_to_day_of_year(self.date_list), self.days_list)
+
+    def test_str_to_day_of_year(self):
+        self.assertEqual(dt.str_to_day_of_year(self.str_date_list[0], self.format), self.days_list[0])
+        self.assertEqual(dt.str_to_day_of_year(self.str_date_list[1], self.format), self.days_list[1])
+        self.assertEqual(dt.str_to_day_of_year(self.str_date_list[2], self.format), self.days_list[2])
+        self.assertListEqual(dt.str_to_day_of_year(self.str_date_list, self.format), self.days_list)
 
 
 if __name__ == '__main__':
