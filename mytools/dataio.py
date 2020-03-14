33eb193332086f158f3c26192acc3966315be086
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-14 23:40:21 +0100

    [lib] avoid the regional data when loading

--- mytools/dataio.py
+++ mytools/dataio.py
@@ -21,15 +21,24 @@ def get_filename_recovered_cases() -> str:
 def load_cases(file_name: str, countries: List[str]=None) -> pd.DataFrame:
     df_cases = pd.read_csv(file_name)
 
+    first_date = '1/22/20'
+    country_col = 'Country/Region'
+    province_col = 'Province/State'
+
     if countries is None:
-        cases_countries = df_cases.loc[:, '1/22/20':].transpose(copy=True)
-        countries_columns = df_cases.loc[:, 'Country/Region'].tolist()
+        cases_countries = df_cases.loc[:, first_date:].transpose(copy=True)
+        countries_columns = df_cases.loc[:, country_col].tolist()
     else:
         # get the rows matching the countries and the columns from the first known datastamp
+
+        # for some countries multiple items are available if regional data is available.
+        # if the region has no regional data the province is empty
+        # otherwise it has the name of the region or the name of the country for the country overall stats
+        condition = ((df_cases[province_col].isin(countries) | pd.isna(df_cases[province_col]) ) & df_cases[country_col].isin(countries))
         # transpose it so that the countries are the columns
-        cases_countries = df_cases[df_cases['Country/Region'].isin(countries)].loc[:, '1/22/20':].transpose(copy=True)
+        cases_countries = df_cases[condition].loc[:, first_date:].transpose(copy=True)
         # reset the name of the columns as the countries
-        countries_columns = df_cases[df_cases['Country/Region'].isin(countries)].loc[:, 'Country/Region'].tolist()
+        countries_columns = df_cases[condition].loc[:, country_col].tolist()
 
     cases_countries.columns = countries_columns
 
