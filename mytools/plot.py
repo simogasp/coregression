0fe6e169009fc25720caf7ff578b1b42201a5f80
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-14 23:42:06 +0100

    [lib] refactoring iplot

--- mytools/plot.py
+++ mytools/plot.py
@@ -1,32 +1,18 @@
 import mytools.date as dt
 
 
-def iplot_comparative_plot(data_frame, title='Comparison of death cases', size=4, yTitle='cases', mode='lines+markers', asFigure=True):
-
-    fig = data_frame.iplot(theme="white", title=title, size=size, yTitle=yTitle, mode=mode,  asFigure=asFigure)
-
-    days = data_frame.index
-
-    fig.update_layout(
-        xaxis=dict(
-            tickmode='array',
-            tickvals=days,
-            ticktext=dt.day_of_year_to_string(days),
-            tickangle=90
-        )
-    )
-
+def iplot_add_log_scale_button(fig):
     fig.update_layout(
         updatemenus=[
             dict(
                 buttons=list([
                     dict(
-                        args=[{"yaxis.type": "linear", "xaxis.type": "linear"}],
+                        args=[{"yaxis.type": "linear"}], #"xaxis.type": "linear"}],
                         label="Linear",
                         method="relayout"
                     ),
                     dict(
-                        args=[{"yaxis.type": "log", "xaxis.type": "log"}],
+                        args=[{"yaxis.type": "log"}], #"xaxis.type": "linear"}],
                         label="Log",
                         method="relayout"
                     )
@@ -43,3 +29,22 @@ def iplot_comparative_plot(data_frame, title='Comparison of death cases', size=4
     )
 
     return fig
+
+
+def iplot_comparative_plot(data_frame, title='Comparison of death cases', size=4, yTitle='cases', mode='lines+markers',
+                           asFigure=True):
+    fig = data_frame.iplot(theme="white", title=title, size=size, yTitle=yTitle, mode=mode, asFigure=asFigure)
+
+    days = data_frame.index
+
+    fig.update_layout(
+        xaxis=dict(
+            tickmode='array',
+            tickvals=days,
+            ticktext=dt.day_of_year_to_string(days),
+            tickangle=90
+        )
+    )
+
+    return iplot_add_log_scale_button(fig)
+
