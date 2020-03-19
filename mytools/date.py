49e2970733b615e2f423383986a4727afc462257
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-19 01:07:37 +0100

    [lib] added str_convert_date and format strings

--- mytools/date.py
+++ mytools/date.py
@@ -1,6 +1,11 @@
 import datetime
-from typing import Union, Iterable as typeIterable
+from typing import Union, List, Iterable as typeIterable
 from collections.abc import Iterable
+
+format_ISO8601 = '%Y-%m-%d %H:%M:%S'
+format_mmddyy = '%m/%d/%y'
+format_ddmmyy = '%d/%m/%y'
+format_mmmdd = '%d %b'
 
 
 def day_of_year_to_date(day: Union[int, float, typeIterable], year: Union[int, float] = None) -> Union[
@@ -23,7 +28,7 @@ def day_of_year_to_date(day: Union[int, float, typeIterable], year: Union[int, f
         return datetime.datetime(int(year), 1, 1) + datetime.timedelta(days=int(day - 1))
 
 
-def date_to_string(date: Union[datetime.datetime, typeIterable[datetime.datetime]], date_format: str = '%d %b'):
+def date_to_string(date: Union[datetime.datetime, typeIterable[datetime.datetime]], date_format: str = format_mmmdd):
     if isinstance(date, Iterable):
         return list(date_to_string(d, date_format) for d in date)
     else:
@@ -39,7 +44,27 @@ def date_to_day_of_year(date: Union[datetime.datetime, typeIterable[datetime.dat
         return delta.days + 1
 
 
-def str_to_day_of_year(date: Union[str, list], date_format: str = '%m/%d/%y') -> Union[int, list]:
+def str_convert_date(date: Union[str, List[str]], format_from: str, format_to: str) -> Union[str, List[str]]:
+    if isinstance(date, list):
+        dates = list(str_convert_date(d, format_from, format_to) for d in date)
+        return dates
+    else:
+        return datetime.datetime.strptime(date, format_from).strftime(format_to)
+
+
+def str_convert_mdy_to_dmy(date: Union[str, List[str]]) -> Union[str, List[str]]:
+    """
+
+    Args:
+        date (str or list of str): the date(s) in mm/dd/yy format
+
+    Returns:
+        dates in dd/mm/yy
+    """
+    return str_convert_date(date, format_from=format_mmddyy, format_to=format_ddmmyy)
+
+
+def str_to_day_of_year(date: Union[str, list], date_format: str = format_mmddyy) -> Union[int, list]:
     if isinstance(date, list):
         days = list(str_to_day_of_year(d, date_format) for d in date)
         return days
@@ -48,7 +73,7 @@ def str_to_day_of_year(date: Union[str, list], date_format: str = '%m/%d/%y') ->
         return date_to_day_of_year(d)
 
 
-def day_of_year_to_string(day: Union[int, float, typeIterable], date_format: str = '%d %b') -> Union[
+def day_of_year_to_string(day: Union[int, float, typeIterable], date_format: str = format_mmmdd) -> Union[
         str, typeIterable[str]]:
     if isinstance(day, Iterable):
         return list(day_of_year_to_string(d, date_format) for d in day)
