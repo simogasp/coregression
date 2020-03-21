29615b7e19c1d1c60d21986a7cc1ced3ab96e4f6
Simone Gasparini <simone.gasparini@gmail.com>
2020-03-21 17:10:34 +0100

    [test] add test for italy_get_list_of_regions()

--- test/test_dataio.py
+++ test/test_dataio.py
@@ -5,12 +5,19 @@ import mytools.dataio as io
 class TesterItaly(unittest.TestCase):
 
     def setUp(self):
+        self.regions_of_italy = ['Abruzzo', 'Basilicata', 'P.A. Bolzano', 'Calabria', 'Campania', 'Emilia Romagna',
+                                 'Friuli Venezia Giulia', 'Lazio', 'Liguria', 'Lombardia', 'Marche', 'Molise',
+                                 'Piemonte', 'Puglia', 'Sardegna', 'Sicilia', 'Toscana', 'P.A. Trento', 'Umbria',
+                                 "Valle d'Aosta", 'Veneto']
         self.provinces_veneto = ['Verona', 'Vicenza', 'Padova', 'Rovigo', 'Venezia', 'Treviso', 'Belluno']
         self.provinces_veneto.sort()
 
     def test_provinces_of_regions(self):
         provinces = io.italy_get_list_of_provinces_for_region('Veneto')
         self.assertListEqual(provinces, self.provinces_veneto)
+
+    def test_provinces_of_italy(self):
+        self.assertListEqual(io.italy_get_list_of_regions(), self.regions_of_italy)
 
 
 if __name__ == '__main__':
